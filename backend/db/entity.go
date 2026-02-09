04468b70a0771979edbe099b2a30a7934a08b2df
lexyblazy <adekola.olalekan01@gmail.com>
2026-02-08 23:25:01 -0600

    add entity definition for balance snapshot

--- backend/db/entity.go
+++ backend/db/entity.go
@@ -11,13 +11,25 @@ type AddressRegistryEntity struct {
 }
 
 type TokenEntity struct {
-	ChainID               int64      `json:"chain_id"`                 // required
-	TokenAddress          string     `json:"token_address"`            // required
-	Symbol                *string    `json:"symbol"`                   // nullable
-	Name                  *string    `json:"name"`                     // nullable
-	Decimals              *int8      `json:"decimals"`                 // nullable
-	FirstSeenBlock        int64      `json:"-"`         // required
-	MetadataFetchFailedAt *time.Time `json:"-"` // nullable
-	CreatedAt             time.Time  `json:"-"`	
+	ChainID               int64      `json:"chain_id"`      // required
+	TokenAddress          string     `json:"token_address"` // required
+	Symbol                *string    `json:"symbol"`        // nullable
+	Name                  *string    `json:"name"`          // nullable
+	Decimals              *int8      `json:"decimals"`      // nullable
+	FirstSeenBlock        int64      `json:"-"`             // required
+	MetadataFetchFailedAt *time.Time `json:"-"`             // nullable
+	CreatedAt             time.Time  `json:"-"`
 	UpdatedAt             time.Time  `json:"-"`
+}
+
+type BalanceSnapshotEntity struct {
+	ID             int64     `json:"id"`
+	ChainID        int64     `json:"chain_id"`
+	WalletAddress  string    `json:"wallet_address"`
+	AssetType      string    `json:"asset_type"`
+	AssetAddress   *string   `json:"asset_address"` // nullable
+	BalanceRaw     string    `json:"balance_raw"`
+	BlockNumber    int64     `json:"block_number"`
+	BlockTimestamp time.Time `json:"block_timestamp"`
+	CreatedAt      time.Time `json:"-"`
 }
