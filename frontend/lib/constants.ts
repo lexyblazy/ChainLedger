4431ea168019672214af3b1f00ed7dda80e328ba
lexyblazy <adekola.olalekan01@gmail.com>
2026-02-12 16:27:47 -0600

    add constants file containing chains config

---
+++ frontend/lib/constants.ts
@@ -0,0 +1,14 @@
+export const CHAINS = {
+  1: {
+    name: "Ethereum Mainnet",
+    chainId: 1,
+    explorerUrl: "https://etherscan.io",
+    ingestionStartDate: "January 1, 2025",
+  },
+  8453: {
+    name: "Base Mainnet",
+    chainId: 8453,
+    explorerUrl: "https://basescan.org",
+    ingestionStartDate: "February 10, 2026",
+  },
+};
